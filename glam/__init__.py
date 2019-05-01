783d0695a6fa783a654ec5786a138acdb33c8f59
Pradyumna <pradyumna@AHBM-PRADYUMNA.local>
2019-05-01 23:55:52 +0100

    1st Models test

---
+++ glam/__init__.py
@@ -0,0 +1,7 @@
+from . import utils
+from . import components
+from . import simulation
+from . import driftSim
+from .models import make_models, GLAM
+from .fit import fit_models
+from .plots import plot_fit
